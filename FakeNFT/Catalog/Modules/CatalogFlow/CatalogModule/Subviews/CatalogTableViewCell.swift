3483f06e6e655ab78eab95a81b2e3e7b77c39931
Ekaterina <katiai@yandex.ru>
2023-07-31 06:54:05 +0300

    CatalogTableViewCellViewModel/cell was added

--- FakeNFT/Catalog/Modules/CatalogFlow/CatalogModule/Subviews/CatalogTableViewCell.swift
+++ FakeNFT/Catalog/Modules/CatalogFlow/CatalogModule/Subviews/CatalogTableViewCell.swift
@@ -5,11 +5,44 @@ struct CatalogTableViewCellViewModel {
     let nftName: String
     let nftCount: String
     
+    private let nftCategoryCover: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(systemName: "")
+        return imageView
+    }()
+    
+    private let nftCategoryLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 12)
+        return label
+    }()
+    
 //    init(nftCodable: NftCodable) {
 //        imageStringUrls = []
 //        nftName = nftCodable.name
 //        nftCount = nftCodable.count
 //    }
+    
+    // MARK: - Properties
+    
+    static let identifier = "catalogCell"
+    
+    private func setupContent() {
+        
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            //nftCategoryCover
+            
+            //nftCategoryLabel
+            
+            ])
+    }
+    
+    
 }
 
 final class CatalogTableViewCell: UITableViewCell {
