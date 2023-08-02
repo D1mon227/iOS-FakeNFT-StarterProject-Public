1624a35c2bc7d72dc718ecadc0627ba74a12d198
Ekaterina <katiai@yandex.ru>
2023-08-02 10:44:01 +0300

    sprint_19

--- FakeNFT/Modules/CatalogFlow/CatalogModule/Subviews/CatalogTableViewCell.swift
+++ FakeNFT/Modules/CatalogFlow/CatalogModule/Subviews/CatalogTableViewCell.swift
@@ -12,10 +12,11 @@ struct CatalogTableViewCellViewModel {
 
 final class CatalogTableViewCell: UITableViewCell {
     
+    // MARK: - Layout elements
+    
     private let nftCategoryCover: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.image = UIImage(systemName: "")
         return imageView
     }()
     
@@ -26,24 +27,52 @@ final class CatalogTableViewCell: UITableViewCell {
         return label
     }()
     
-    private func setupContent() {
+    // MARK: - Properties
+    
+    var identifier = "CatalogTableViewCell"
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
         
+        setupContent()
+        setupConstraints()
     }
     
-    private func setupConstraints() {
-        NSLayoutConstraint.activate([
-            //nftCategoryCover
-            
-            //nftCategoryLabel
-            
-            ])
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Methods
     
     func configure(with viewModel: CatalogTableViewCellViewModel) {
-        viewModel.imageStringUrl
+        nftCategoryCover.image = UIImage(named: "\(viewModel.imageStringUrl)")
         nftCategoryLabel.text = viewModel.nftTitle
         
     }
+    
 }
 
+// MARK: - Layout methods
+
+private extension CatalogTableViewCell {
+    func setupContent() {
+        selectionStyle = .none
+        contentView.addSubview(nftCategoryCover)
+        contentView.addSubview(nftCategoryLabel)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            // nftCategoryCover
+            nftCategoryCover.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            nftCategoryCover.topAnchor.constraint(equalTo: contentView.topAnchor),
+            nftCategoryCover.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            nftCategoryCover.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            // nftCategoryLabel
+            nftCategoryLabel.topAnchor.constraint(equalTo: nftCategoryCover.bottomAnchor),
+            nftCategoryLabel.trailingAnchor.constraint(equalTo: nftCategoryCover.trailingAnchor)
+        ])
+    }
+}
