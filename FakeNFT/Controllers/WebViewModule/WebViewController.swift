b734c8b65af5222413f3914dd430e53243d7fbc6
Ekaterina <katiai@yandex.ru>
2023-08-07 18:51:35 +0300

    Добавлен экран webview с информацией об авторе

---
+++ FakeNFT/Controllers/WebViewModule/WebViewController.swift
@@ -0,0 +1,70 @@
+//
+//  WebViewController.swift
+//  FakeNFT
+//
+//  Created by Екатерина Иванова on 07.08.2023.
+//
+
+import UIKit
+import WebKit
+
+protocol WebViewControllerProtocol: AnyObject {
+    func load(request: URLRequest)
+}
+
+final class WebViewController: UIViewController, WebViewControllerProtocol {
+    //MARK: - Properties
+    private let webView = WKWebView()
+
+    //MARK: - presenter
+    private let presenter: WebViewPresenterProtocol
+    
+    init(presenter: WebViewPresenterProtocol) {
+        self.presenter = presenter
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupConstraints()
+        webView.navigationDelegate = self
+        presenter.viewDidLoad()
+    }
+
+    //MARK: - Helpers
+    
+    func load(request: URLRequest) {
+        webView.load(request)
+    }
+    
+    private func setupConstraints() {
+        
+        view.addSubview(webView)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
+    }
+}
+
+//MARK: - WKNavigationDelegate
+extension WebViewController: WKNavigationDelegate {
+    func webView(
+        _ webView: WKWebView,
+        decidePolicyFor navigationAction: WKNavigationAction,
+        decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
+    ) {
+        decisionHandler(.allow)
+    }
+
+}
+
