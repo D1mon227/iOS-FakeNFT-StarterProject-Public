4fc0b8776d2fddf4f09bd175bdfea5ea8e3e4a3d
desikvasya <desikvasya1@gmail.com>
2023-08-07 16:59:29 +1000

    Добавил сортировку для корзины

--- FakeNFT/Controllers/Cart/CartViewController.swift
+++ FakeNFT/Controllers/Cart/CartViewController.swift
@@ -1,23 +1,25 @@
 import UIKit
 import Kingfisher
 
-final class CartViewController: UIViewController, UITableViewDataSource {
+final class CartViewController: UIViewController, UITableViewDataSource{
 
     // MARK: - Properties
 
     private var containerView: UIView!
     var cartArray: [CartStruct] = []
+    
+    var viewModel: CartModelProtocol?
+
 
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
+
         super.viewDidLoad()
         addPlaceholder()
         setupView()
         setupNavigationBar()
         cartArray = CartMockData.shared.mockCart
-
-//        cartArray = CartMockData.shared.mockCart
         cartTable.dataSource = self
     }
 
@@ -27,17 +29,30 @@ final class CartViewController: UIViewController, UITableViewDataSource {
         view.backgroundColor = .white
         view.addSubview(payButton)
         view.addSubview(cartTable)
-
+        view.addSubview(cartInfo)
+        view.addSubview(countOfNFTS)
+        view.addSubview(priceOfNFTS)
+        
         NSLayoutConstraint.activate([
             payButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
             payButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
             payButton.widthAnchor.constraint(greaterThanOrEqualToConstant: 240),
             payButton.heightAnchor.constraint(equalToConstant: 44),
-
+            
             cartTable.topAnchor.constraint(equalTo: view.topAnchor),
             cartTable.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             cartTable.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            cartTable.bottomAnchor.constraint(equalTo: payButton.topAnchor, constant: -16)
+            cartTable.bottomAnchor.constraint(equalTo: payButton.topAnchor, constant: -16),
+
+            cartInfo.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            cartInfo.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            cartInfo.heightAnchor.constraint(equalToConstant: 76),
+            
+            countOfNFTS.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            countOfNFTS.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -40),
+            
+            priceOfNFTS.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            priceOfNFTS.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
         ])
 
         cartTable.separatorStyle = .none
@@ -69,17 +84,47 @@ final class CartViewController: UIViewController, UITableViewDataSource {
         table.translatesAutoresizingMaskIntoConstraints = false
         return table
     }()
+    
+    let countOfNFTS: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let priceOfNFTS: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 15, weight: .bold)
+        label.numberOfLines = 0
+        label.textColor = UIColor(red: 0.11, green: 0.62, blue: 0, alpha: 1)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let cartInfo: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 0.97, green: 0.97, blue: 0.97, alpha: 1)
+        view.layer.cornerRadius = 12
+        view.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
 
     private func setupNavigationBar() {
         if let navBar = navigationController?.navigationBar {
             let sortButton = UIButton(type: .custom)
             sortButton.setImage(UIImage(named: "Vector"), for: .normal)
             sortButton.frame = CGRect(x: 0, y: 0, width: 24, height: 24)
+            sortButton.addTarget(self, action: #selector(sortButtonTapped), for: .touchUpInside)
 
             let imageBarButtonItem = UIBarButtonItem(customView: sortButton)
             navBar.topItem?.setRightBarButton(imageBarButtonItem, animated: false)
         }
     }
+
 
     // MARK: - Appearance
 
@@ -92,30 +137,129 @@ final class CartViewController: UIViewController, UITableViewDataSource {
             placeholderView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
     }
+    
+    @objc private func sortButtonTapped() {
+        showMenu()
+    }
+
+    private func showMenu() {
+        let alertController = UIAlertController(title: "Sort Cart", message: nil, preferredStyle: .actionSheet)
+        
+        for sortingOption in Sort.allCases {
+            if sortingOption != .byTitle && sortingOption != .close && sortingOption != .byNFTQuantity {
+                let action = UIAlertAction(title: sortingOption.localizedString, style: .default) { _ in
+                    self.sortBy(option: sortingOption)
+                }
+                alertController.addAction(action)
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Close", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+
+
+
+    private func sortBy(option: Sort) {
+        switch option {
+        case .byName:
+            cartArray = cartArray.sorted(by: { $0.nftName < $1.nftName })
+            cartTable.reloadData()
+        case .byPrice:
+            cartArray = cartArray.sorted(by: { $0.nftPrice > $1.nftPrice })
+            cartTable.reloadData()
+        case .byRating:
+            cartArray = cartArray.sorted(by: { $0.nftRating > $1.nftRating })
+            cartTable.reloadData()
+        default:
+            break
+        }
+    }
+
+
+
+    
+    private func fillInfo() {
+        countOfNFTS.text = "\(cartArray.count) NFT"
+        var price = 0.0
+        cartArray.forEach { cart in
+            price += cart.nftPrice
+        }
+        priceOfNFTS.text = "\(String(format: "%.2f", price)) ETH"
+    }
 
     // MARK: - UITableViewDataSource
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        fillInfo()
+        if cartArray.isEmpty {
+            tableView.isHidden = true
+            cartInfo.isHidden = true
+            cartTable.isHidden = true
+            countOfNFTS.isHidden = true
+            priceOfNFTS.isHidden = true
+            payButton.isHidden = true
+        } else {
+            tableView.isHidden = false
+            cartInfo.isHidden = false
+            cartTable.isHidden = false
+            countOfNFTS.isHidden = false
+            priceOfNFTS.isHidden = false
+            payButton.isHidden = false
+        }
         return cartArray.count
     }
 
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cartCell", for: indexPath) as? CartCell else {
+//            fatalError("Error: Unable to dequeue CartCell")
+//        }
+//        let nft = cartArray[indexPath.row]
+//        cell.nftName.text = nft.nftName
+//        cell.nftPrice.text = "\(nft.nftPrice) ETH"
+//        cell.setupRating(rating: nft.nftRating)
+//
+//        if let imageURL = URL(string: nft.nftImages.first ?? "") {
+//            cell.nftImage.kf.setImage(with: imageURL)
+//        }
+//
+//        cell.indexCell = indexPath.row
+//        cell.delegate = self
+//
+//        return cell
+//    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cartCell", for: indexPath) as? CartCell else {
-            fatalError("Error: Unable to dequeue CartCell")
-        }
-        let nft = cartArray[indexPath.row]
-        cell.nftName.text = nft.nftName
-        cell.nftPrice.text = "\(nft.nftPrice) ETH"
-        cell.setupRating(rating: nft.nftRating)
-
-        if let imageURL = URL(string: nft.nftImages.first ?? "") {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "cartCell", for: indexPath) as? CartCell else {
+                return UITableViewCell() // Return a fallback cell if needed
+            }
+            let rating = cartArray[indexPath.row].nftRating
+            let name = cartArray[indexPath.row].nftName
+            let price = "\(cartArray[indexPath.row].nftPrice) ETH"
+            let imageURL = URL(string: cartArray[indexPath.row].nftImages.first ?? "")
+            cell.setupRating(rating: rating)
+            cell.nftName.text = name
+            cell.nftPrice.text = String(price)
             cell.nftImage.kf.setImage(with: imageURL)
+            cell.indexCell = indexPath.row
+            cell.delegate = self
+            return cell
         }
-
-        cell.indexCell = indexPath.row
-        cell.delegate = self
-
-        return cell
+    
+    
+    
+    
+    private func getData(ids: [String]) {
+        ids.forEach { id in
+            viewModel?.getNFT(nftID: id, completion: { cart in
+                self.cartArray.append(cart)
+                DispatchQueue.main.async {
+                    self.cartTable.reloadData()
+                }
+            })
+        }
     }
 
     // MARK: - Actions
@@ -168,3 +312,17 @@ extension UIView {
         return vStack
     }
 }
+
+// MARK: - Extension for UITableViewDelegate
+extension CartViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
+    
+}
+
+
+    
+
+
