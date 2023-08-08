2fcaf38fe3385fab9e6e06cae69d69318cedb56d
Ekaterina <katiai@yandex.ru>
2023-08-08 07:11:55 +0300

    cell elements

--- FakeNFT/Modules/CatalogFlow/DetailedCollectionModule/Subviews/NFTCollectionTableViewCellModel.swift
+++ FakeNFT/Modules/CatalogFlow/DetailedCollectionModule/Subviews/NFTCollectionTableViewCellModel.swift
@@ -1,6 +1,17 @@
 
 import Foundation
 
-final class NFTCollectionTableViewCellModel {
+struct NFTCollectionTableViewCellModel {
+    let id: String
+    let website: URL?
+    let name: String
+    let nftsCollection: [String] = []
+    let description: String = ""
     
+    
+    init(authorResponse: AuthorResponse) {
+        self.id = authorResponse.id
+        self.website = authorResponse.website.makeUrl()
+        self.name = authorResponse.name
+    }
 }
