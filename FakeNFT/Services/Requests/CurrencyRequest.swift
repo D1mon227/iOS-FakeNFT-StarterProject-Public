7f29d36bfc987087857d75e2ad82efc026559855
Dmitry <d1mon227@yandex.ru>
2023-08-16 20:45:57 +0300

    добавлен enum с path для всех запросов

--- FakeNFT/Services/Requests/CurrencyRequest.swift
+++ FakeNFT/Services/Requests/CurrencyRequest.swift
@@ -2,7 +2,7 @@ import Foundation
 
 struct CurrencyRequest: NetworkRequest {
     var endpoint: URL? {
-        URL(string: Resourses.BaseURL.baseURL + "currencies")
+        URL(string: Resourses.Network.baseURL + Resourses.Network.Paths.currencies)
     }
     let httpMethod: HttpMethod
     let dto: Encodable?
