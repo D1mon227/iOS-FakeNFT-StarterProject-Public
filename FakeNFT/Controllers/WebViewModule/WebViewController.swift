1f231eb6b5f86b81b34ec6485778605a546df5f8
Ekaterina <katiai@yandex.ru>
2023-08-17 09:43:51 +0300

    Добавлен индикатор загрузки ветвью

--- FakeNFT/Controllers/WebViewModule/WebViewController.swift
+++ FakeNFT/Controllers/WebViewModule/WebViewController.swift
@@ -10,15 +10,22 @@ import WebKit
 
 protocol WebViewControllerProtocol: AnyObject {
     func load(request: URLRequest)
+    func setProgressValue(_ newValue: Float)
+    func setProgressHidden(_ isHidden: Bool)
 }
 
 final class WebViewController: UIViewController, WebViewControllerProtocol {
+    
     //MARK: - Properties
     private let webView = WKWebView()
-
+    private let progressView = UIProgressView(progressViewStyle: .bar)
+    
+    private var estimatedProgressObservation: NSKeyValueObservation?
+    
     //MARK: - presenter
     private let presenter: WebViewPresenterProtocol
     
+    //MARK: - LifeCycle
     init(presenter: WebViewPresenterProtocol) {
         self.presenter = presenter
         super.init(nibName: nil, bundle: nil)
@@ -34,8 +41,40 @@ final class WebViewController: UIViewController, WebViewControllerProtocol {
         setupConstraints()
         webView.navigationDelegate = self
         presenter.viewDidLoad()
+        setProgressView()
+        
+        estimatedProgressObservation = webView.observe(
+            \.estimatedProgress,
+             options: [],
+             changeHandler: { [weak self] _, _ in
+                 guard let self = self else { return }
+                 self.presenter.didUpdateProgressValue(self.webView.estimatedProgress)
+             }
+        )
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        webView.addObserver(
+            self,
+            forKeyPath: #keyPath(WKWebView.estimatedProgress),
+            options: .new,
+            context: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), context: nil)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == #keyPath(WKWebView.estimatedProgress) {
+            presenter.didUpdateProgressValue(webView.estimatedProgress)
+        } else {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+        }
+    }
+    
     //MARK: - Helpers
     
     func load(request: URLRequest) {
@@ -43,7 +82,7 @@ final class WebViewController: UIViewController, WebViewControllerProtocol {
     }
     
     private func setupConstraints() {
-		view.backgroundColor = UIColor.backgroundDay
+        view.backgroundColor = UIColor.backgroundDay
         view.addSubview(webView)
         webView.translatesAutoresizingMaskIntoConstraints = false
         
@@ -51,7 +90,31 @@ final class WebViewController: UIViewController, WebViewControllerProtocol {
             webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             webView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+    
+    //MARK: - Methods
+    
+    func setProgressValue(_ newValue: Float) {
+        progressView.setProgress(newValue, animated: true)
+    }
+    
+    func setProgressHidden(_ isHidden: Bool) {
+        progressView.isHidden = isHidden
+    }
+    
+    func setProgressView() {
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        progressView.setProgress(0, animated: false)
+        progressView.tintColor = .blueUniversal
+        view.addSubview(progressView)
+        
+        NSLayoutConstraint.activate([
+            progressView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            progressView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            progressView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            progressView.heightAnchor.constraint(equalToConstant: 2)
         ])
     }
 }
@@ -65,6 +128,4 @@ extension WebViewController: WKNavigationDelegate {
     ) {
         decisionHandler(.allow)
     }
-
 }
-
