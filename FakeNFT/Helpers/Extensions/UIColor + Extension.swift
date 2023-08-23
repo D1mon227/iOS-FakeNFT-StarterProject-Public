579fb30a426f4f9c6d02c6c516cee1063d40b967
Dmitry <d1mon227@yandex.ru>
2023-08-23 16:05:00 +0300

    clean files and groups

---
+++ FakeNFT/Helpers/Extensions/UIColor + Extension.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+extension UIColor {
+    static var backgroundDay: UIColor { UIColor(named: "Background [day]") ?? .white}
+    static var blackDay: UIColor { UIColor(named: "Black [day]") ?? .black}
+    static var blueUniversal: UIColor { UIColor(named: "Blue Universal") ?? .blue}
+    static var greenUniversal: UIColor { UIColor(named: "Green Universal") ?? .green}
+    static var lightGreyDay: UIColor { UIColor(named: "Light grey [day]") ?? .lightGray}
+    static var lightGrey: UIColor { UIColor(named: "Light grey") ?? .lightGray}
+    static var yellowUniversal: UIColor { UIColor(named: "Yellow Universal") ?? .yellow}
+    static var redUniversal: UIColor { UIColor(named: "Red Universal") ?? .red}
+    static var textViewPlaceholder: UIColor { UIColor(named: "textViewPlaceholderColor") ?? .lightGray}
+}
