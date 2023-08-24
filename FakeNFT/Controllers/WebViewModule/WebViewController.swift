146819d2618b741d60bcc8c9e65489b7b827a827
Dmitry <d1mon227@yandex.ru>
2023-08-24 20:00:14 +0300

    fix

--- FakeNFT/Controllers/WebViewModule/WebViewController.swift
+++ FakeNFT/Controllers/WebViewModule/WebViewController.swift
@@ -1,10 +1,3 @@
-//
-//  WebViewController.swift
-//  FakeNFT
-//
-//  Created by Екатерина Иванова on 07.08.2023.
-//
-
 import UIKit
 import WebKit
 
--- FakeNFT/Controllers/WebViewModule/WebViewPresenter.swift
+++ FakeNFT/Controllers/WebViewModule/WebViewPresenter.swift
@@ -1,10 +1,3 @@
-//
-//  WebViewPresenter.swift
-//  FakeNFT
-//
-//  Created by Екатерина Иванова on 07.08.2023.
-//
-
 import UIKit
 
 protocol WebViewPresenterProtocol {
